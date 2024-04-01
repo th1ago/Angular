f0ad35e3b7cc2620e86705723243d24c9856f504
Thiago <thiago_murakami@hotmail.com>
2024-04-01 18:45:43 -0300

    feat: criando o component btn

--- landing-page/src/app/components/home/home.component.ts
+++ landing-page/src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    BtnPrimaryComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
